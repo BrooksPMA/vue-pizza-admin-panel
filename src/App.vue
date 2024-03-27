a2e67d57aa52e6a75b1cb4c0d4c7ffa09afe93f6
BrooksPMA <68705027+BrooksPMA@users.noreply.github.com>
2024-03-27 19:33:18 +0300

    bug fixes

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,29 @@
 <template>
-  <sidebar v-if="isLoggedIn"></sidebar>
-  <div :class="[isLoggedIn ? 'hasSidebar' : '']">
-    <router-view></router-view>
-  </div>
+    <sidebar v-if="isLoggedIn"></sidebar>
+    <div :class="[isLoggedIn ? 'hasSidebar' : '']">
+        <router-view></router-view>
+    </div>
 </template>
 
 <script setup>
-import { computed } from 'vue';
-import Sidebar from './components/Sidebar.vue';
-import { useAuthStore } from './stores/authStore';
+import { computed, onUnmounted } from "vue";
+import Sidebar from "./components/Sidebar.vue";
+import { useAuthStore } from "./stores/authStore";
 
 const authStore = useAuthStore();
 const isLoggedIn = computed(() => {
-  return authStore.isLoggedIn;
+    return authStore.isLoggedIn;
+});
+
+onUnmounted(() => {
+    console.log("unmounted");
+    authStore.logout();
 });
 </script>
 
 <style scoped>
 .hasSidebar {
-  padding: 1rem;
-  margin-left: 16rem;
+    padding: 1rem;
+    margin-left: 16rem;
 }
 </style>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,35 +1,35 @@
 <template>
-  <div class="flex mb-8">
-    <div
-      v-for="foodName in foodNames"
-      :key="foodName.key"
-      class="px-4 py-1 cursor-pointer"
-    >
-      <router-link :to="{ name: link, params: { type: foodName.name } }">
-        {{ foodName.title }}</router-link
-      >
+    <div class="flex mb-8">
+        <div
+            v-for="foodName in foodNames"
+            :key="foodName.key"
+            class="px-4 py-1 cursor-pointer"
+        >
+            <router-link :to="{ name: link, params: { type: foodName.name } }">
+                {{ foodName.title }}</router-link
+            >
+        </div>
     </div>
-  </div>
 </template>
 
 <script setup>
-import { reactive } from 'vue';
+import { reactive } from "vue";
 
 defineProps({
-  link: String,
+    link: String,
 });
 
 const foodNames = reactive([
-  { title: 'Пицца', name: 'pizza' },
-  { title: 'Закуски', name: 'snacks' },
-  { title: 'Дессерты', name: 'desserts' },
-  { title: 'Напитки', name: 'drinks' },
-  { title: 'Другие товары', name: 'others' },
+    { title: "Пицца", name: "pizza" },
+    { title: "Закуски", name: "snacks" },
+    { title: "Дессерты", name: "desserts" },
+    { title: "Напитки", name: "drinks" },
+    { title: "Другие товары", name: "others" },
 ]);
 </script>
 
 <style scoped>
 .active {
-  border-bottom: 2px solid rgb(31 41 55);
+    border-bottom: 2px solid rgb(31 41 55);
 }
 </style>
